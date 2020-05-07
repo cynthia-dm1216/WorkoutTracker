1252aeebdf179b2a0cdf445efbb3d0afe95f7c66
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-07 16:28:32 -0500

    added html and api routes

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-let db = require("../models");
+let db = require("./models");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
@@ -134,7 +134,7 @@ let workoutSeed = [
     ]
   }
 ];
-
+console.log(db);
 db.Workout.deleteMany({})
   .then(() => db.Workout.collection.insertMany(workoutSeed))
   .then(data => {
--- server.js
+++ server.js
@@ -1,36 +1,71 @@
-// const express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
-// const path = require('path');
-// const logger = require('morgan');
+const path = require('path');
+const logger = require('morgan');
 
 const PORT = process.env.PORT || 5000;
 
-const db = require('../models');
+const db = require('./models');
 
-// const app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",{useNewUrlParser:true}); 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
-// API route GET /api/workouts
-app.get("/api/workouts", (req,res) => {
-db.Workout.find({})
-  .then(dbWorkout => {
-      res.json(dbWorkout);
-  })
-  .catch(err => {
-      res.json(err);
-  });
+
+// API Routes - /excercise , '/', '/stats'
+app.get("/excercise", (req, res) => {
+    res.sendFile(path.join(__dirname + './public/exercise.html'))
 });
 
-// API excercise route - /excercise
-app.get("/excercise", (req,res) => {
-   res.sendFile(path.join(__dirname, ''))
+app.get('/',(req,res) => {
+    res.sendFile([path.join(__dirname + "./public/index.html")])
+});
+
+app.get('/stats', (req,res) => {
+    res.sendFile(path.join(__dirname + "./public/stats.html"))
+});
+
+// API route GET /api/workouts
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+
+app.post("/api/workouts", (req,res) => {
+    db.Workout.create({})
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.json(err)
+    })
+});
+
+app.put("/api/workouts/:id", (req,res) => {
+    console.log(req.body);
 })
+
 
 app.listen(PORT, () => {
     console.log(`App running on port ${5000}!`);
 });
+
