1acce173cafb724f2789de2c3bfe4ed0c429ccc7
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-07 12:08:13 -0500

    added workout schema table

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -3,15 +3,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Workout Schema
-const WorkoutSchema = new Schema ({
-    day: Date,
-    totalDuration: {
+const workout = new Schema ({
+ day: {
+     type: Date,
+     dafault: () => new Date
+ },
+ exercises: [{
+     type: {
+         type: String,
+         trim: true,
+         require: "Enter an exercise type:"
+     },
+     name: {
+         type: String,
+         trim: true,
+         rewuire: "Enter exercise type:"
+     },
+     duration: {
         type: Number,
-        default: 0
-    }
+        require: "Enter duration time:"
+     },
+     weigth: {
+         type: Number
+     },
+     reps: {
+         type: Number
+     },
+     sets: {
+         type: Number,
+     },
+     distance: {
+         type: Number
+     }
+     
+ }]
 });
 
 //exporting Wowrkout
-const Workout = mongoose.model("Workout",WorkoutSchema);
+const Workout = mongoose.model("Workout",workout);
 
 module.exports = Workout;
