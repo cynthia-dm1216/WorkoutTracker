7a81a7c57b27d667cf5ee071c6476efd49065982
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-08 15:08:35 -0500

    connecting to mongotdb

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+//Connecting to MongoDB
+mongoose.connect(MONGODB_URI);
 
 // API route GET /api/workouts
 app.get("/api/workouts", (req, res) => {
